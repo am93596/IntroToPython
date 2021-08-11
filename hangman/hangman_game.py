d97d6628c88ea55c6d73f1f4cc97ab88150428ea
Amy Murphy <amurphy@spartaglobal.com>
2021-08-11 11:05:12 +0100

    updated hangman game - everything complete except while loop for main game

--- hangman/hangman_game.py
+++ hangman/hangman_game.py
@@ -2,91 +2,79 @@ import random
 from hangman_words import word_list
 
 
+# FUNCTION DEFINITIONS
+
+
 # function that randomly selects a word
 def randomly_select_word():
     return random.choice(word_list)
 
 
-# TODO: function that outputs game
-# might want to put the main function calls in here,
-# then call this through main?
-def output_game(word):
-    print("_" * (len(word)))
+def find_letters_in_word(letter_guess, game_word):
+    letter_positions = []
+    for index, char in enumerate(game_word):
+        if char == letter_guess:
+            letter_positions.append(index)
+    return letter_positions
+
+
+def process_guess(letter, game_word, updated_board):
+    positions = find_letters_in_word(letter, game_word)
+    if positions:
+        for p in positions:
+            updated_board = updated_board[:p] + letter + updated_board[p+1:]
+        return updated_board
+    else:
+        print("Wrong! Guess again.")
+        return updated_board
 
 
 # function that takes input guesses
-def input_guesses():
-    guess = input("What is your guess?\n")
-    verify_input(guess)
+def input_guess(used_guesses):
+    input_verified = False
+    guess = None
+    while not input_verified:
+        guess = input("What is your guess?\n")
+        if verify_input(guess) and not already_used(guess, used_guesses):
+            input_verified = True
+            guess = guess.upper()
     return guess
 
 
 # function that verifies the input is 1 letter
 def verify_input(guess):
-    print("Checking input...")
+    print("Checking input...\n")
     if len(guess) != 1 or guess.isalpha() is False:
-        print("Please enter a single letter as your guess")
-    else:
-        print("Thanks!")
-
-
-# function that identifies where a given letter appears in a given word
-# and returns either False or a list of locations
-def find_letter_in_word(guess, word):
-    guess_locations = []
-    index = 0
-    for letter in word:
-        if letter == guess.upper():
-            guess_locations.append(index)
-        index += 1
-    if len(guess_locations) < 1:
+        print("Please enter a single letter as your guess\n")
         return False
     else:
-        return guess_locations
+        return True
 
 
 # function that checks if letter has already been guessed
 def already_used(guess, used_guesses):
-    if guess in used_guesses:
-        print("Sorry, you've already guessed that letter. Try again")
+    if guess.upper() in used_guesses:
+        print("Sorry, you've already guessed that letter. Try again\n")
         return True
     else:
-        print("Great! That letter hasn't been guessed yet!")
+        print("Great! That letter hasn't been guessed yet!\n")
         return False
 
 
-# TODO: function that adds stuff to hangman when a guess is wrong
+# function that reduces lives when a guess is wrong
+def reduce_lives(current_lives):
+    return current_lives - 1
 
-# function that reveals correctly guessed letters - except that sometimes (not always!!!) it does something wrong
-def reveal_correct_guess(locations, answer):
-    updated_word = ""
-    # This is the loop where it keeps going wrong! I want it to only add '_' for unsolved letters
-    # sometimes it does
-    # sometimes it does too many
-    # e.g. for STERNUTATION, and input 't', printed out: _T____TT
-    # never mind how to get this to work when some letters have been filled in
-    # fix that later
-    for index in locations:
-        print(f"updated_word index: {index-len(updated_word)}")  # debugging line
-        updated_word += ("_" * (index-len(updated_word)))
-        updated_word += answer[index]
-        #updated_word_length += len(index-updated_word_length)
-        # for sublist in updated_word: this for loop is turning out wrong - replace it with something that
-        #                              updates the length of updated_word
-        #     print(f"sublist: {sublist}")
-        #     updated_word_length += len(sublist)
-    print(len(answer))  # debugging line
-    print(len(updated_word))  # debugging line
-    print(updated_word)
-    if len(updated_word) < len(answer):  # fails this test if 1st letter is guessed, as 1st element '' is counted as 1
-        print(f"Difference: {len(answer)-len(updated_word)}")
-        updated_word += "_" * (len(answer)-len(updated_word))
-    print(f"Final: {updated_word}")
-    # string_version_updated_word = ""
-    # for sublist in updated_word:
-    #     for elem in sublist:
-    #         string_version_updated_word += '_'.join(str(elem))
-    # for sublist in updated_word:
-    #     print(f"Sublist: {sublist}")
-    #     string_version_updated_word = "".join(sublist)
-    # print(f"Final: {string_version_updated_word}")
+
+# GAME RUNS FROM HERE
+word = randomly_select_word()
+board = '_' * len(word)
+lives = 3
+already_guessed = []
+print(board)
+
+next_guess = input_guess(already_guessed)
+board = process_guess(next_guess, word, board)
+print(f"Board: {board}")
+print(f"Lives: {lives}")
+print(word)
--- hangman/play_hangman_game.py
+++ hangman/play_hangman_game.py
@@ -1,19 +1,1 @@
-from hangman_game import randomly_select_word, output_game, input_guesses, find_letter_in_word, already_used, reveal_correct_guess
 
-
-# TODO: main function to run the game
-def main():
-    previous_guesses = []
-    random_word = randomly_select_word()
-    print(random_word)
-    output_game(random_word)
-    guess = input_guesses()
-    if not already_used(guess, previous_guesses):
-        previous_guesses.append(guess)
-        correct_guess_locations = find_letter_in_word(guess, random_word)
-        print(correct_guess_locations)
-        reveal_correct_guess(correct_guess_locations, random_word)
-    # else prompt again
-
-
-main()
